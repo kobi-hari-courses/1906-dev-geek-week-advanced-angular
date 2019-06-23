c9c83adcf6713fa5a7e7b89be1c5b00c37a97064
Kobi Hari <kobi2294@yahoo.com>
2019-06-23 15:38:16 +0300

    Layout of app-root and toggling of preview area

--- tv-shows/src/app/app.component.ts
+++ tv-shows/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tv-shows';
+  isShowingPreview = true;
+
+  togglePreviow() {
+    this.isShowingPreview = !this.isShowingPreview;
+  }
 }
